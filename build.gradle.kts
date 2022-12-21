ea159503b38079a2f28062eafbca0a16311c63d7
Astralchroma <astralchroma@proton.me>
2022-12-21 22:33:52 +0000

    build to a jar lol

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
-	@Suppress("DSL_SCOPE_VIOLATION") // TODO: https://youtrack.jetbrains.com/issue/KTIJ-19369
 	alias(libs.plugins.kotlin)
+	alias(libs.plugins.shadow)
+	application
 }
+
+version = "1.1.0"
 
 repositories {
 	mavenCentral()
@@ -13,3 +17,7 @@ dependencies {
 }
 
 kotlin.jvmToolchain(17)
+
+application {
+	mainClass.set("io.github.petercrawley.aggregator.Aggregator")
+}
